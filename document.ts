2cafc058769cd76470db2478158d3140388f034b
Marcos Candeia <marrcooos@gmail.com>
2024-10-02 19:51:41 -0300

    Fix to use version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- document.ts
+++ document.ts
@@ -5,33 +5,47 @@ import { default as fjp } from "fast-json-patch";
 export interface IDocument<TDocument> {
     patch: (
         ops: fjp.Operation[],
-    ) => Promise<{ conflict: boolean; content: TDocument }>;
-    get: () => TDocument | null;
-    watch: () => AsyncIterableIterator<TDocument>;
+    ) => Promise<{ conflict: boolean; state: DocumentState<TDocument> }>;
+    get: () => DocumentState<TDocument> | null;
+    watch: () => AsyncIterableIterator<DocumentState<TDocument>>;
 }
 
+export interface DocumentState<T> {
+    doc: T | null;
+    version: number;
+}
 export class Document<T> implements IDocument<T> {
-    private _state: T | null;
-    private watchTarget = new WatchTarget<T>();
+    private _state: DocumentState<T> | null;
+    private watchTarget = new WatchTarget<DocumentState<T>>();
 
     constructor(protected state: ActorState) {
         this._state = null;
         state.blockConcurrencyWhile(async () => {
             this._state = await state.storage.get("state");
+            this._state ??= { doc: null, version: 0 } as DocumentState<T>;
         });
     }
     async patch(
         ops: fjp.Operation[],
-    ): Promise<{ conflict: boolean; content: T }> {
-        const initialState = this._state ?? {} as T;
+    ): Promise<{ conflict: boolean; state: DocumentState<T> }> {
+        const initialState = this._state ??
+            { doc: null, version: 0 } as { doc: T; version: number };
         try {
+            const doc = ops.reduce<T>(
+                fjp.applyReducer,
+                initialState.doc ?? {} as T,
+            );
+            const nextState = {
+                doc,
+                version: initialState.version + 1,
+            };
             const result = {
                 conflict: false as const,
-                content: ops.reduce<T>(fjp.applyReducer, initialState),
+                state: nextState,
             };
-            await this.state.storage.put("state", result.content);
-            this._state = result.content;
-            this.watchTarget.notify(result.content);
+            await this.state.storage.put("state", nextState);
+            this._state = result.state;
+            this.watchTarget.notify(this._state);
             return result;
         } catch (error) {
             if (
@@ -40,16 +54,16 @@ export class Document<T> implements IDocument<T> {
             ) {
                 return {
                     conflict: true as const,
-                    content: initialState,
+                    state: initialState,
                 };
             }
             throw error;
         }
     }
-    get(): T | null {
+    get(): DocumentState<T> | null {
         return this._state;
     }
-    watch(): AsyncIterableIterator<T> {
+    watch(): AsyncIterableIterator<DocumentState<T>> {
         return this.watchTarget.subscribe();
     }
 }
