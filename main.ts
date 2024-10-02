157af7fb9249bbe9c10f66529b1fac8fc58a8ac0
viktormarinho <viktormpcs@gmail.com>
2024-10-01 23:19:14 -0300

    presence

--- main.ts
+++ main.ts
@@ -1,15 +1,34 @@
 import { ActorRuntime } from "@deco/actors";
-import { Counter } from "./counter.ts";
+import { Presence } from "./presence.ts";
 const portEnv = Deno.env.get("PORT");
 const port = portEnv ? +portEnv : 8000;
 
-const rt = new ActorRuntime([Counter]);
+const rt = new ActorRuntime([Presence]);
 
 Deno.serve({
-  handler: (req) => {
+  handler: async (req) => {
     const url = new URL(req.url);
+
+    if (req.method === "OPTIONS") {
+      const response = new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Max-Age": "86400",
+        },
+      });
+      return response;
+    }
+
     if (url.pathname.startsWith("/actors")) {
-      return rt.fetch(req);
+      const response = await rt.fetch(req);
+      response.headers.set("Access-Control-Allow-Origin", "*");
+      response.headers.set("Access-Control-Allow-Headers", "*");
+      response.headers.set("Access-Control-Allow-Methods", "*");
+      response.headers.set("Access-Control-Allow-Credentials", "true");
+      response.headers.set("Access-Control-Max-Age", "86400");
+      return response;
     }
     return new Response(null, { status: 200 });
   },
---
+++ presence.ts
@@ -0,0 +1,67 @@
+import { ActorState } from "@deco/actors";
+import { WatchTarget } from "@deco/actors/watch";
+
+interface User {
+    id: string;
+    name: string;
+    country: string;
+    x: number;
+    y: number;
+}
+
+type UserId = string;
+
+interface State {
+    users: Record<UserId, User>;
+}
+
+interface IPresence {
+    join: (userId: string) => AsyncIterableIterator<State>;
+    update: (userId: string, ctx: { x: number; y: number }) => void;
+}
+
+export class Presence implements IPresence {
+    private _state: State = {
+        users: {},
+    };
+    private watchTarget = new WatchTarget<State>();
+
+    constructor(protected state: ActorState) {
+        state.blockConcurrencyWhile(async () => {});
+    }
+
+    update(userId: string, ctx: { x: number; y: number }): void {
+        this._state.users[userId].x = ctx.x;
+        this._state.users[userId].y = ctx.y;
+
+        this.watchTarget.notify(this._state);
+    }
+
+    join(userId: string): AsyncIterableIterator<State> {
+        let leave = () => {};
+
+        this._state.users[userId] ??= {
+            id: userId,
+            name: "Anonymous",
+            country: "Unknown",
+            x: 0,
+            y: 0,
+        };
+        const previous = leave;
+        leave = () => {
+            delete this._state.users[userId];
+            this.watchTarget.notify(this._state);
+            previous();
+        };
+
+        const subscribe = this.watchTarget.subscribe();
+        this.watchTarget.notify(this._state);
+        const retn = subscribe.return;
+        subscribe.return = function (val) {
+            leave();
+            return retn?.call(subscribe, val) ?? val;
+        };
+
+        return subscribe;
+    }
+}
